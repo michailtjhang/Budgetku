b6f447f85145c3d500478f96aa33302f919f7fd5
michailtjhang <michailaja@gmail.com>
2024-11-01 21:51:02 +0700

    initial Project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Counter;
+use App\Models\Product;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Counter::factory(1)->create();
+        // Product::factory(5)->create();
+
+    }
+}
